297f4cfe439de53483eb95c5904d46ede5076914
Raffy Sucilan <raffy.sucilan@adapthealth.com>
2026-01-31 06:01:52 +0800

    Redirect to .xyz domain when accessed through .playit.plus domain.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$playitDomain = 'mbgplayground.playit.plus';
+if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === $playitDomain){
+    header('Location: https://mbgplayground.xyz' . $_SERVER['REQUEST_URI'], true, 301);
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
