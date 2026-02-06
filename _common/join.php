14fc1d9b1d204aab34c908f445e51c6ba68cd524
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-06 22:00:48 +0800

    Add tooltip for Discord invite text link.

--- _common/join.php
+++ _common/join.php
@@ -1,4 +1,4 @@
 <ul>
-	<li>Join our <span class="highlight"><a href="discord">Discord</a></span> server and navigate to <span class="highlight">Hosted Server</span> category.</li>
+	<li>Join our <span class="highlight"><a href="discord" title="Discord Invite Link">Discord</a></span> server and navigate to <span class="highlight">Hosted Server</span> category.</li>
 	<li>All information required to connect is listed in the pinned post of the server channel.</li>
 </ul>
