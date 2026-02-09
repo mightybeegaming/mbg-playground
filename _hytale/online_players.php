6b84217ce8b82cca80663ecc3bdb34caa989edd3
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-10 04:40:15 +0800

    Update new regex for checking Hytale online players.

--- _hytale/online_players.php
+++ _hytale/online_players.php
@@ -2,7 +2,7 @@
 $file = 'online_players.txt';
 $content = file_get_contents('_hytale/online_players.txt');
 
-preg_match('/default\s*\((\d+)\)/', $content, $matches);
+preg_match('/^[^(]*\((\d+)\)/', $content, $matches);
 
 $online_players = $matches[1] ?? 0;
 
