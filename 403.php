01d24104c81a616cc52625f2ebaf6c2d9dd84bbd
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-10 23:00:06 +0800

    Implement custom error pages.

---
+++ 403.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>MBG 403</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="/_common/icon.png" type="image/png">
+        <link rel="stylesheet" href="/_common/style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="section" style="text-align:center">
+                <h1>403 - Forbidden</h1>
+                <p>You don’t have permission to access this page.</p>
+            </div>
+        </div>
+        <?php include '_common/footer.php'?>
+		<?php include '_common/gtag.php'?>
+    </body>
+</html>
---
+++ 404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>MBG 404</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="/_common/icon.png" type="image/png">
+		<link rel="stylesheet" href="/_common/style.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="section" style="text-align:center">
+				<h1>404 - Page Not Found</h1>
+				<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+			</div>
+		</div>
+		<?php include '_common/footer.php'?>
+		<?php include '_common/gtag.php'?>
+	</body>
+</html>
