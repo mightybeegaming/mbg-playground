22e2d67fad91fef5e0a5b6cae8767cbb3b3f76cd
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-18 01:45:50 +0800

    Apply defined constants to custom error pages.

--- _404.php
+++ _404.php
@@ -1,12 +1,11 @@
-<?php define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'])?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>MBG 404</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="icon" href="/_common/icon.png" type="image/png">
-		<link rel="stylesheet" href="/_common/style.css">
+		<link rel="icon" href="<?=URL_MBGPLAYGROUNDLOGO?>" type="image/png">
+		<link rel="stylesheet" href="<?=URL_CSS?>">
 	</head>
 	<body>
 		<div class="container">
@@ -15,7 +14,7 @@
 				<p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
 			</div>
 		</div>
-		<?php include ROOT_DIR. '/_common/footer.php'?>
-		<?php include ROOT_DIR. '/_common/gtag.php'?>
+		<?php include PATH_FOOTER?>
+		<?php include PATH_GOOGLETAG?>
 	</body>
 </html>
