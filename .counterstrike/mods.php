ba5b242c3985bcfa50fe097a1540abf7af4a5574
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-24 05:34:49 +0800

    Create separate mod pages for Counter-Strike and V Rising instead of using .md files.

---
+++ .counterstrike/mods.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>MBG Counter-Strike Mods</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="/_media/mbgplaygroundlogo.png" type="image/png">
+		<link rel="stylesheet" href="/_common/style.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="section">
+				<h1>MBG Counter-Strike Mods</h1>
+				<p>Mod collection for the Counter-Strike dedicated server.</p>
+			</div>
+			<div class="section">
+				<h1>Mod Collection</h1>
+				<ul>
+					<li><a href="https://www.amxmodx.org" target="_blank">AMX Mod X</a></li>
+					<li><a href="https://github.com/rehlds/ReGameDLL_CS/tree/master/regamedll/extra/HostageImprov" target="_blank">CS:CZ Hostage AI</a></li>
+					<li><a href="https://github.com/rehlds/Metamod-R" target="_blank">Metamod-R</a></li>
+					<li><a href="https://github.com/APGRoboCop/podbot_mm" target="_blank">PODBot</a></li>
+					<li><a href="https://github.com/rehlds/ReGameDLL_CS" target="_blank">ReGameDLL_CS </a></li>
+					<li><a href="https://github.com/rehlds/ReHLDS" target="_blank">ReHLDS</a></li>
+					<li><a href="https://forums.alliedmods.net/showthread.php?t=66006</a>" target="_blank">Ultimate Sounds</li>
+				</ul>
+			</div>
+		</div>
+		<footer>
+			<?php include '../_common/licensing.php'?>
+		</footer>
+		<?php include '../_common/gtag.php'?>
+	</body>
+</html>
---
+++ .vrising/mods.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>MBG V Rising Mods</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="/_media/mbgplaygroundlogo.png" type="image/png">
+		<link rel="stylesheet" href="/_common/style.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="section">
+				<h1>MBG V Rising Mods</h1>
+				<p>Mod collection for the V Rising dedicated server.</p>
+			</div>
+			<div class="section">
+				<h1>Mod Collection</h1>
+				<ul>
+					<li><a href="https://thunderstore.io/c/v-rising/p/skytech6/AutoBrazier" target="_blank">AutoBrazier</a></li>
+					<li><a href="https://thunderstore.io/c/v-rising/p/Midon/AutoCloseDoors" target="_blank">AutoCloseDoors</a></li>
+					<li><a href="https://thunderstore.io/c/v-rising/p/zfolmt/Bloodcraft" target="_blank">Bloodcraft</a></li>
+					<li><a href="https://thunderstore.io/c/v-rising/p/zfolmt/Eclipse" target="_blank">Eclipse</a></li>
+					<li><a href="https://thunderstore.io/c/v-rising/p/Kindred/KindredLogistics" target="_blank">KindredLogistics</a></li>
+					<li><a href="https://thunderstore.io/c/v-rising/p/ScarletMods/ScarletMarket" target="_blank">ScarletMarket</a></li>
+				</ul>
+			</div>
+		</div>
+		<footer>
+			<?php include '../_common/licensing.php'?>
+		</footer>
+		<?php include '../_common/gtag.php'?>
+	</body>
+</html>
