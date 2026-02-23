d4688817c10a37dc13161b35d5be624c3296af2a
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-24 06:01:13 +0800

    Fix incorrect path for Downloads page.

--- .common/bootstrap.php
+++ .common/bootstrap.php
@@ -15,7 +15,7 @@ define('PATH_ONLINEPROJECTZOMBOID', PATH_ROOT . '/.onlineplayers/projectzomboid.
 define('PATH_ONLINEVRISING', PATH_ROOT . '/.onlineplayers/vrising.txt');
 
 define('URL_MBGPLAYGROUND', 'https://mbgplayground.xyz');
-define('URL_DOWNLOADS', URL_MBGPLAYGROUND. '/downloads');
+define('URL_DOWNLOADS', URL_MBGPLAYGROUND. '/.downloads');
 define('URL_COUNTERSTRIKE', URL_MBGPLAYGROUND. '/counterstrike');
 define('URL_DISCORD', URL_MBGPLAYGROUND. '/discord');
 define('URL_HYTALE', URL_MBGPLAYGROUND. '/hytale');
