12ba7f641d201bb38cb2ad9cb1afb775e9b73173
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-02-28 03:35:21 +0800

    Adjust font weight on some labels.

--- .common/join.php
+++ .common/join.php
@@ -1,5 +1,5 @@
 <p><span class="highlight">How to join</span></p>
 <ul class="list-decimal">
-    <li>Join our <a href="/discord" title="Discord Invite Link">Discord</a> server and navigate to <span class="highlight">Dedicated Server</span> category.</li>
+    <li>Join our <b><a href="/discord" title="Discord Invite Link">Discord</a></b> server and navigate to <span class="highlight">Dedicated Server</span> category.</li>
     <li>All information required to connect is listed in the pinned post of the server channel.</li>
 </ul>
