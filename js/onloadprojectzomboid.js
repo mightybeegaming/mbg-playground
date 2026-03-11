e405f92920cbe7b1671641a2e7e427a68652dd7a
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-03-11 23:44:39 +0800

    Separate info boxes.

--- js/onloadprojectzomboid.js
+++ js/onloadprojectzomboid.js
@@ -2,34 +2,22 @@ async function loadServerMetrics() {
     const request = await fetch('/php/server.php?method=getMetricsProjectZomboid', {cache: 'no-store'});
     const data = await request.json();
     // console.log(data);
-
-    let worldAge = '';
-    if(data.worldAge && data.season) {
-        worldAge += `<span class="highlight">${data.season}</span>`;
-        worldAge += '<b> | </b>';
-        worldAge += `<span class="highlight">${data.worldAge} days</span>`;
-    }
-
+    
     let dateTime = '';
     if(data.worldDate && data.worldTime) {
-        dateTime += `<span class="highlight">${data.worldDate}</span>`;
+        dateTime += '<span class="highlight">' + data.worldDate + '</span>';
         dateTime += '<b> | </b>';
-        dateTime += `<span class="highlight">${data.worldTime}</span>`;
-    }
-
-    let weatherTemperature = '';
-    if(data.weather && data.temperature) {
-        weatherTemperature = `<span class="highlight">${data.weather}</span>`;
-        weatherTemperature += '<b> | </b>';
-        weatherTemperature += `<span class="highlight">${data.temperature} °C</span>`;
+        dateTime += '<span class="highlight">' + data.worldTime + '</span>';
     }
 
     document.getElementById('statusText').textContent = data.server.statusText;
-    document.getElementById('uptime24').textContent = `${data.server.uptime24} %`;
-    document.getElementById('onlinePlayers').textContent = `${data.onlinePlayers} / 100`;
-    document.getElementById('worldAge').innerHTML = worldAge;
+    document.getElementById('uptime24').textContent = data.server.uptime24 + ' %';
+    document.getElementById('onlinePlayers').textContent = data.onlinePlayers + '/ 100';
+    document.getElementById('worldAge').textContent = data.worldAge + ' days';
     document.getElementById('dateTime').innerHTML = dateTime;
-    document.getElementById('weatherTemperature').innerHTML = weatherTemperature;
+    document.getElementById('season').textContent = data.season;
+    document.getElementById('weather').textContent = data.weather;
+    document.getElementById('temperature').textContent = data.temperature + ' °C';
 }
 setInterval(loadServerMetrics, 5000);
 loadServerMetrics();
