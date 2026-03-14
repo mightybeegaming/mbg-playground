a658ba3296318991e54d325d5dbbf77e8bfb7046
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-03-14 08:54:18 +0800

    Remove traces of time info.

--- php/server.php
+++ php/server.php
@@ -178,8 +178,7 @@ class Server {
         $metricsValheim = [
             'server' => '',
             'onlinePlayers' => 0,
-            'worldAge' => 0,
-            'time' => 0
+            'worldAge' => 0
         ];
 
         $fileContent = file_get_contents('../metrics/online' . $game . '.txt');
@@ -189,11 +188,6 @@ class Server {
         
         preg_match('/Day\s+(\d+)/', $fileContent, $match);
         $metricsValheim['worldAge'] = $match[1];
-
-        preg_match('/Current server time:\s*([\d\.]+)/', $fileContent, $match);
-        $seconds = (float)$match[1];
-        $time = date("g:i A", $seconds);
-        $metricsValheim['time'] = $time;
 
         $config = $this->getConfig($game . '.json');
         if($config) $metricsValheim = array_merge($metricsValheim, $config);
