3385877e60c15c5ff2b8a3e3ee1d2d5fbe2af00c
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-08-04 07:46:20 +0800

    Add Palworld link to Nav Bar.

--- php/page.php
+++ php/page.php
@@ -96,6 +96,7 @@ class Page {
         $navBar .= '<p>';
         $navBar .= '<a href="/counterstrike">Counter-Strike</a>&ensp;&ensp;';
         $navBar .= '<a href="/hytale">Hytale</a>&ensp;&ensp;';
+        $navBar .= '<a href="/palworld">Palworld</a>&ensp;&ensp;';
         $navBar .= '<a href="/projectzomboid">Project&nbsp;Zomboid</a>&ensp;&ensp;';
         $navBar .= '<a href="/vrising">V&nbsp;Rising</a>&ensp;&ensp;';
         $navBar .= '<a href="/valheim">Valheim</a>';
