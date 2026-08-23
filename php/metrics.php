eb6ee2fd6dae3d424adf6675e92231ef935d128a
Raffy Sucilan <raffy.sucilan@gmail.com>
2026-08-24 04:05:36 +0800

    Disable metrics mapper for Counter-Strike.

--- php/metrics.php
+++ php/metrics.php
@@ -19,7 +19,7 @@ class Metrics {
 
         $this->metricsMapper = [
             'all' => 'getAll',
-            'counterStrike' => 'getCounterStrike',
+            // 'counterStrike' => 'getCounterStrike',
             // 'hytale' => 'getHytale',
             'palworld' => 'getPalworld',
             'projectZomboid' => 'getProjectZomboid',
